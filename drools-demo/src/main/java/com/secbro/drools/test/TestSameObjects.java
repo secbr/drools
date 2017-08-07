a30b3c41daa71d5b834cf8cc03b8d7c8cc812a1c
secbr <xinyoulingxi2008@126.com>
2017-08-07 15:11:04 +0800

    Comparing two instances of same objects in Drools by list

--- drools-demo/src/main/java/com/secbro/drools/test/TestSameObjects.java
+++ drools-demo/src/main/java/com/secbro/drools/test/TestSameObjects.java
@@ -5,6 +5,9 @@ import com.secbro.drools.model.Customer;
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhuzs on 2017/8/7.
  */
@@ -21,6 +24,26 @@ public class TestSameObjects extends BaseTest {
         Customer customer1 = new Customer();
         customer1.setAge(59);
         kieSession.insert(customer1);
+
+        int count = kieSession.fireAllRules();
+
+        kieSession.dispose();
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    @Test
+    public void testSameObjectsInList() {
+        KieSession kieSession = getKieSession("two same objects in list");
+
+        List<Customer> list = new ArrayList<>();
+        Customer customer = new Customer();
+        customer.setAge(61);
+        list.add(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setAge(59);
+        list.add(customer1);
+        kieSession.insert(list);
 
         int count = kieSession.fireAllRules();
 
