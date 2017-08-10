6a5e5dcd26dcd4a4ccff87f4eab795e1ba9c4744
secbr <xinyoulingxi2008@126.com>
2017-08-11 07:51:12 +0800

    add the example 'get name and package in the rules'

---
+++ drools-demo/src/main/java/com/secbro/drools/test/TestGetNameAndPackage.java
@@ -0,0 +1,21 @@
+package com.secbro.drools.test;
+
+import com.secbro.drools.BaseTest;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * Created by zhuzs on 2017/8/11.
+ */
+public class TestGetNameAndPackage extends BaseTest{
+
+    @Test
+    public void test(){
+        KieSession kieSession = this.getKieSession("Name and package");
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rule(s)!");
+
+    }
+}
