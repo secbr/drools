ef163eef30b35a39bd559b60e952150717006f56
secbr <xinyoulingxi2008@126.com>
2017-09-17 21:45:28 +0800

    example of FactHandler

---
+++ drools-lesson/src/main/java/com/secbro/drools/chapter4/FactHandlerTest.java
@@ -0,0 +1,36 @@
+package com.secbro.drools.chapter4;
+
+import com.secbro.drools.BaseTest;
+import com.secbro.drools.model.Person;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+import javax.swing.*;
+
+public class FactHandlerTest extends BaseTest{
+
+    @Test
+    public void testFactHandler(){
+        KieSession kieSession = this.getKieSession("fact-handler-group");
+
+        Person p = new Person();
+        p.setAge(81);
+
+        FactHandle handle = kieSession.insert(p);
+
+        System.out.println(handle.toExternalForm());
+
+        int count = kieSession.fireAllRules();
+        System.out.println("Fires " + count + " rules!");
+
+        p.setAge(90);
+        kieSession.getAgenda().getAgendaGroup("fact-handler-group").setFocus();
+        kieSession.update(handle,p);
+
+        count =  kieSession.fireAllRules();
+
+        System.out.println("Fires " + count + " rules!");
+        kieSession.dispose();
+    }
+}
