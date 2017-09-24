d6081140d5a7ff65dc56662aaf351d6271c32589
secbr <xinyoulingxi2008@126.com>
2017-09-24 18:01:28 +0800

    example of StatelessKieSession

--- drools-lesson/src/main/java/com/secbro/drools/BaseTest.java
+++ drools-lesson/src/main/java/com/secbro/drools/BaseTest.java
@@ -3,6 +3,7 @@ package com.secbro.drools;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
 
 /**
  * Created by zhuzs on 2017/8/9.
@@ -20,6 +21,19 @@ public class BaseTest {
     protected KieSession getKieSession(String agendaGroupName){
         KieSession kieSession  = getKieSession();
         kieSession.getAgenda().getAgendaGroup(agendaGroupName).setFocus();
+        return kieSession;
+    }
+
+    protected StatelessKieSession getStatelessKieSession(){
+        KieServices kieServices = KieServices.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        StatelessKieSession kieSession = kieContainer.newStatelessKieSession("stateless-rules");
+
+        return kieSession;
+    }
+
+    protected StatelessKieSession getStatelessKieSession(String agendaGroupName){
+        StatelessKieSession kieSession  = getStatelessKieSession();
         return kieSession;
     }
 
---
+++ drools-lesson/src/main/java/com/secbro/drools/chapter5/StatelessSessionTest.java
@@ -0,0 +1,27 @@
+package com.secbro.drools.chapter5;
+
+import com.secbro.drools.BaseTest;
+import com.secbro.drools.model.Person;
+import org.junit.Test;
+import org.kie.api.runtime.StatelessKieSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatelessSessionTest extends BaseTest{
+
+    @Test
+    public void testStateLessSession(){
+        StatelessKieSession kieSession = this.getStatelessKieSession();
+        List<Person> list = new ArrayList<>();
+
+        Person p = new Person();
+        p.setAge(35);
+        list.add(p);
+        Person p1 = new Person();
+        p1.setAge(20);
+        list.add(p1);
+//        kieSession.execute(p);
+        kieSession.execute(list);
+    }
+}
