8ad01ef3c4dbcf7212a3192b433c6f35948ec77f
secbr <xinyoulingxi2008@126.com>
2017-11-01 07:59:08 +0800

    usage of timer

---
+++ drools-lesson/src/main/java/com/secbro/drools/model/Server.java
@@ -0,0 +1,24 @@
+package com.secbro.drools.model;
+
+public class Server {
+
+    private int times;
+
+    private String result;
+
+    public int getTimes() {
+        return times;
+    }
+
+    public void setTimes(int times) {
+        this.times = times;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
