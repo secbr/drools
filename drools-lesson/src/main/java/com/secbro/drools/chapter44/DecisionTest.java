7c6bd4ac27792fe912ba86b85cb9f8bf62770e84
zhu <541075754@qq.com>
2017-12-17 15:01:41 +0800

    usage of decision tables

--- drools-lesson/src/main/java/com/secbro/drools/chapter44/DecisionTest.java
+++ drools-lesson/src/main/java/com/secbro/drools/chapter44/DecisionTest.java
@@ -4,6 +4,8 @@ import com.secbro.drools.BaseTest;
 import org.drools.decisiontable.InputType;
 import org.drools.decisiontable.SpreadsheetCompiler;
 import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,5 +24,21 @@ public class DecisionTest extends BaseTest{
         SpreadsheetCompiler compiler = new SpreadsheetCompiler();
         String drl = compiler.compile(is, InputType.XLS);
         System.out.println(drl);
+    }
+
+    @Test
+    public void checkDrl2() throws FileNotFoundException {
+        SpreadsheetCompiler compiler = new SpreadsheetCompiler();
+        String drl = compiler.compile(ResourceFactory.newClassPathResource("com/decision/decision.xls"), InputType.XLS);
+        System.out.println(drl);
+    }
+
+    @Test
+    public void testDecision(){
+
+        KieSession kieSession = this.getKieSessionBySessionName("decision-rules");
+
+        kieSession.fireAllRules();
+        kieSession.dispose();
     }
 }
