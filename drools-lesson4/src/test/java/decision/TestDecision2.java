0e04aa72137f3a089be57175585815557c272ea3
secbro <xinyoulingxi2008@126.com>
2020-03-14 17:50:40 +0800

    decision tables

--- drools-lesson4/src/test/java/decision/TestDecision2.java
+++ drools-lesson4/src/test/java/decision/TestDecision2.java
@@ -3,8 +3,12 @@ package decision;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.kie.api.KieServices;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+
+import java.util.List;
 
 /**
  * @author sec
@@ -18,10 +22,20 @@ public class TestDecision2 {
 	public void testDecision() {
 
 		KieContainer container = KieServices.get().getKieClasspathContainer();
-		KieSession kieSession = container.newKieSession();
+
+		Results results = container.verify();
+		if (results.hasMessages(Message.Level.ERROR)) {
+			List<Message> messages = results.getMessages();
+			for (Message message : messages) {
+				log.info("错误信息:{}", message);
+			}
+		}
+
+
+		KieSession kieSession = container.newKieSession("decision-rules");
 
 		int count = kieSession.fireAllRules();
-		log.info("Fired {} rules!",count);
+		log.info("Fired {} rules!", count);
 		kieSession.dispose();
 	}
 }
