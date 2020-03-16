17d20aa365f41044674a8ca3733c06e546127fb6
secbro <xinyoulingxi2008@126.com>
2020-03-16 16:22:45 +0800

    Import

---
+++ drools-lesson4/src/main/java/com/choupangxia/entity/Person.java
@@ -0,0 +1,17 @@
+package com.choupangxia.entity;
+
+import lombok.Data;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2020/3/16 3:56 PM
+ **/
+@Data
+public class Person {
+
+	private String name;
+
+	private int age;
+
+}
