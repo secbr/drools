dd5cc3721b8eb25fbc5336c0a985abaf92d60baa
secbro <xinyoulingxi2008@126.com>
2020-03-16 15:23:56 +0800

    decision tables

---
+++ drools-lesson4/doc/doc.md
@@ -0,0 +1,38 @@
+## RuleSet区域
+- RuleSet
+- Sequential
+- SequentialMaxPriority
+- SequentialMinPriority
+- EscapeQuotes
+- NumericDisabled
+- Import
+- Variables
+- Functions
+- Queries
+- Declare
+
+### 规则属性
+
+- PRIORITY
+- DURATION
+- TIMER
+- ENABLED
+- CALENDARS
+- NO-LOOP
+- LOCK-ON-ACTIVE
+- AUTO-FOCUS
+- ACTIVATION-GROUP
+- AGENDA-GROUP
+- RULEFLOW-GROUP
+
+## RuleTable区域
+
+- NAME
+- DESCRIPTION
+- CONDITION
+- ACTION
+- METADATA
+
+
+
+
---
+++ drools-lesson4/src/test/java/decision/TestDecision3.java
@@ -0,0 +1,50 @@
+package decision;
+
+import lombok.extern.slf4j.Slf4j;
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+
+import java.util.List;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2020/3/12 10:51 AM
+ **/
+@Slf4j
+public class TestDecision3 {
+
+	@Test
+	public void checkDrl2() {
+		SpreadsheetCompiler compiler = new SpreadsheetCompiler();
+		String drl = compiler.compile(ResourceFactory.newClassPathResource("com/decision2/decision2.xls"),
+				InputType.XLS);
+		System.out.println(drl);
+	}
+
+	@Test
+	public void testDecision() {
+
+		KieContainer container = KieServices.get().getKieClasspathContainer();
+		Results results = container.verify();
+		if (results.hasMessages(Message.Level.ERROR)) {
+			List<Message> messages = results.getMessages();
+			for (Message message : messages) {
+				log.info("错误信息:{}", message);
+			}
+		}
+
+		KieSession kieSession = container.newKieSession("decision-rules2");
+
+		int count = kieSession.fireAllRules();
+		log.info("Fired {} rules!", count);
+		kieSession.dispose();
+	}
+}
