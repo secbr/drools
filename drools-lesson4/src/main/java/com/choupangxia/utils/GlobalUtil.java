a5b92c1305c9bf00cb9841275c1385dff932331f
secbro <xinyoulingxi2008@126.com>
2020-03-19 15:56:56 +0800

    action

--- drools-lesson4/src/main/java/com/choupangxia/utils/GlobalUtil.java
+++ drools-lesson4/src/main/java/com/choupangxia/utils/GlobalUtil.java
@@ -10,8 +10,8 @@ public class GlobalUtil {
 	private static int count = 0;
 
 	public static void add() {
-		System.out.println("GlobalUtil.add()");
 		count++;
+		System.out.println("GlobalUtil.add()---count=" + count);
 	}
 
 	public static int getCount() {
